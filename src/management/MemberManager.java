0ee9c983b5078759f0a18a171310399695fccb27
Alireza <rostmi83@gmail.com>
2025-05-09 13:36:30 +0330

    complete membermanager

--- src/management/MemberManager.java
+++ src/management/MemberManager.java
@@ -1,30 +1,29 @@
 package management;
 
+import datastructures.maps.CustomHashMap;
 import library.Member;
 import library.Transaction;
 
 public class MemberManager {
-    // TODO: Define a data structure that stores memberIds and their matching member objects
+    CustomHashMap<String,Member> memberMap ;
 
     public MemberManager() {
-        // TODO: Initialize your data structure here
+        memberMap = new CustomHashMap<>();
     }
 
     public void addMember(Member member) {
-        // TODO: Add member and their id to your data structure
+       memberMap.put(member.getMemberId(),member);
     }
 
     public Member getMember(String memberId) {
-        // TODO
-        return null;
+        return memberMap.get(memberId);
     }
 
     public void recordTransaction(String memberId, Transaction transaction) {
-        // TODO
+        memberMap.get(memberId).addTransaction(transaction);
     }
 
     public Transaction getLastTransaction(String memberId) {
-        // TODO
-        return null;
+        return memberMap.get(memberId).getLastTransaction();
     }
 }
